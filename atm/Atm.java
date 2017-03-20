370dfa49453e477974c586017e42853e6d8e4ef0
Sergey <sefimenko13@gmail.com>
2017-03-20 17:26:21 +0300

    Update Atm.java

--- atm/Atm.java
+++ atm/Atm.java
@@ -15,11 +15,23 @@ public class Atm{
 	
 	public void insertCard(Card card){
 		if(!cardIn){
+			if(checkPin()){
 		currentCard = card;
 		cardIn = true;
+			} else {System.out.println("Sorry, the PIN is incorrect");}
 		} else {
 			System.out.println("Please eject the current card");
 		}
+	}
+	
+	public boolean checkPin(){
+		
+		System.out.println("Enter the PIN");
+		Scanner sc = new Scanner(System.In);
+		String pinInput = sc.nextLine();
+		
+		if((currentCard.getPin()).equals(pinInput))
+		{return true;} else {return false;}
 	}
 	
 	public void ejectCard(){
